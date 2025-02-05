fb8829f5afadf64b54510b66614b7436870b6d59
sabarixr <143121872+sabarixr@users.noreply.github.com>
2025-02-05 21:17:12 +0530

    changed url in apollo-client.ts

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://root.shuttleapp.rs/",
+  uri: "https://root.amfoss.in/",
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
