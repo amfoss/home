45541e8670512825f5ab9127d96ab9ac29fec21c
Nishthajain7 <jnishtha305@gmail.com>
2025-02-05 17:43:10 +0530

    Fixed responsive issues in landscape view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <LampContainer>
           <p></p>
         </LampContainer>
+        <div>
         <div className="absolute flex justify-between items-center w-full lg:top-[4vh] sm:top-[3vh]">
           <Image className="relative lg:max-w-[3.5vw] md:max-w-[3.5vw] sm:max-w-[10vw] lg:left-[2vw] sm:left-[2.5vw]" src={logo} alt="logo" priority />
           <p className="relative right-[4vw] text-white font-[1000] lg:text-[3.5vh] md:text-[8vh] sm:text-[5vw]">H<span className="text-yellow-800">&lt;&gt;</span>ME</p>
@@ -17,7 +18,7 @@ export default function Home() {
         <p className="absolute lg:top-[45vh] md:top-[35vh] sm:top-[42.5vh] left-[0vw] w-[100vw] animate-fadeInUp lg:text-[3vw] md:text-[4vw] sm:text-[7vw] text-white font-bold opacity-0" style={{ textAlign: "center" }}>India's Leading FOSS Club</p>
         <Image className="absolute animate-fadeInUp lg:max-w-[20vw] md:max-w-[25vw] sm:max-w-[43vw] lg:left-[40vw] md:left-[38vw] sm:left-[28vw] lg:top-[52.5vh] sm:top-[48.3vh] opacity-0" src={amfoss} alt="amfoss" />
         <button className="absolute animate-fadeInUp text-center lg:text-[2.5vh] md:text-xl sm:text-2xl text-black lg:w-[15vw] sm:w-[34vw] md:w-[15vw] sm:h-[4.8vh] lg:h-[5.5vh] md:h-[10vh] bg-yellow-400 lg:rounded-[0.5rem] md:rounded-[0.7rem] sm:rounded-[1rem] font-bold lg:top-[70vh] md:top-[75vh] sm:top-[60vh] lg:left-[43vw] md:left-[43vw] sm:left-[33vw] opacity-0">Sign in</button>
-
+        </div>
       </div>
     </main>
   );
--- src/components/lamp.tsx
+++ src/components/lamp.tsx
@@ -15,7 +15,6 @@ export const LampContainer = ({
       className={cn("relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-bgMainColor w-full z-0",className)}>
       <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
 
-        <div className="absolute lg:top-[35vh] sm:top-[35vh] md:top-[25vh] max-w-[50vw]">
           <motion.div
             initial={{ opacity: 0.5, width: "15rem" }}
             whileInView={{ opacity: 1, width: "30rem" }}
@@ -27,13 +26,11 @@ export const LampContainer = ({
             style={{
               backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
             }}
-            className="absolute right-1/2 lg:h-[30vh] sm:h-[30vh] md:h-[40vh] max-w-[50vw] bg-gradient-conic from-yellow-500 via-transparent to-transparent [--conic-position:from_70deg_at_center_top]"
+            className="lg:top-[35vh] sm:top-[35vh] md:top-[25vh] absolute right-1/2 lg:h-[30vh] sm:h-[30vh] md:h-[40vh] max-w-[50vw] bg-gradient-conic from-yellow-500 via-transparent to-transparent lg:[--conic-position:from_70deg_at_center_top] md:[--conic-position:from_40deg_at_center_top] sm:[--conic-position:from_70deg_at_center_top]"
           >
             <div className="absolute lg:w-40 sm:w-[8.5vw] h-[100%] left-0 bg-bgMainColor  bottom-0 [mask-image:linear-gradient(to_right,white,transparent)]" />
           </motion.div>
-        </div>
 
-        <div className="absolute lg:top-[35vh] sm:top-[35vh] md:top-[25vh]">
           <motion.div
             initial={{ opacity: 0.5, width: "15rem" }}
             whileInView={{ opacity: 1, width: "30rem" }}
@@ -43,13 +40,13 @@ export const LampContainer = ({
               ease: "easeInOut",
             }}
             style={{backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,}}
-            className="absolute left-1/2 lg:h-[30vh] sm:h-[30vh] md:h-[40vh] max-w-[50vw] bg-gradient-conic from-transparent via-transparent to-yellow-500 [--conic-position:from_290deg_at_center_top]"
+            className="absolute lg:top-[35vh] sm:top-[35vh] md:top-[25vh] left-1/2 lg:h-[30vh] sm:h-[30vh] md:h-[40vh] max-w-[50vw] bg-gradient-conic from-transparent via-transparent to-yellow-500 md:[--conic-position:from_320deg_at_center_top] lg:[--conic-position:from_290deg_at_center_top] sm:[--conic-position:from_290deg_at_center_top]"
           >
             <div className="absolute lg:w-40 sm:w-[8rans.5vw] h-[100%] right-0 bg-bgMainColor bottom-0 [mask-image:linear-gradient(to_left,white,transparent)]" />
           </motion.div>
-        </div>
 
-        <div className="absolute top-1/2 h-[18vh] w-full translate-y-12 scale-x-150 bg-bgMainColor blur-2xl"></div>
+        <div className="absolute lg:top-[55vh] md:top-[50vh] sm:top-[50vh] lg:h-[18vh] md:h-[40vh] sm:h-[30vh] w-full scale-x-150 bg-bgMainColor lg:blur-2xl md:blur-xl sm:blur-2xl"></div>
+
         <div className="absolute inset-auto z-50 h-36 w-[23.33vw] -translate-y-1/2 rounded-full bg--500 opacity-50 blur-3xl"></div>
         
         <motion.div
